425423fbef55fb4c0a0d6afae998863da796c967
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-26 18:45:18 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,49 @@
 const CACHE_NAME = "atelier-ppnc-v1";
-const ASSETS = [
+const urlsToCache = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
   "./manifest.json",
+  "./logo-lactalis.png",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
-// Installation du service worker et mise en cache
+// Installation du service worker
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
+  self.skipWaiting();
 });
 
-// Activation - suppression des anciens caches
+// Activation
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
     )
   );
+  self.clients.claim();
 });
 
 // Interception des requêtes réseau
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).then(res => {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, resClone));
+          return res;
+        })
+      );
+    })
   );
 });
