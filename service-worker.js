981c038c5a3fc0d312f58bbe914fd4db8b4a0dab
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-26 20:15:05 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,19 +4,39 @@ const urlsToCache = [
   "./style.css",
   "./app.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/icon-192.png"
 ];
 
-self.addEventListener("install", event => {
+// INSTALLATION
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+// ACTIVATION
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
