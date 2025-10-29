4715ccde169800c9ec7c98787b42eefe5e1b6ece
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-30 00:55:37 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,21 @@
-const CACHE_NAME = "atelier-ppnc-v1";
-const FILES_TO_CACHE = [
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+const CACHE_NAME = 'atelier-ppnc-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
 ];
 
-// Installation et mise en cache
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-// Activation et nettoyage ancien cache
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => {
-        if (k !== CACHE_NAME) return caches.delete(k);
-      }))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', evt => {
+  evt.waitUntil(clients.claim());
 });
-
-// Interception des requêtes
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
   );
 });
