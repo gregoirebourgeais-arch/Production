44e3a99003eb70b2b948d1c397d279f4c1a3844e
gregoirebourgeais-arch <gregoire.bourgeais@gmail.com>
2025-10-30 07:28:46 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,39 @@
-const CACHE_NAME = "atelier-ppnc-v1";
+// Atelier PPNC - Service Worker complet
+
+const CACHE_NAME = 'atelier-ppnc-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-self.addEventListener("install", event => {
+// Installation du service worker et mise en cache
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", event => {
+// Activation et nettoyage des anciens caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
